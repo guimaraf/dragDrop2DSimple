f09d3378d50fca18fb9a330410b16bcad057dca2
guimaraf <guimaraf@gmail.com>
2019-06-26 10:19:04 -0300

    first commit

---
+++ Assets/Script/DragDrop.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class DragDrop : MonoBehaviour
+{
+    private bool onCollider = false, onDrag = false;
+    [SerializeField] private Rigidbody2D rigidbody2D = default;
+    [SerializeField] private Camera mainCamera = default;
+
+    private void OnMouseOver()
+    {
+        onCollider = true;
+    }
+    private void OnMouseExit()
+    {
+        onCollider = false;
+    }
+    void Update()
+    {
+        if(onCollider & Input.GetKeyDown(KeyCode.Mouse0))
+
+        {
+            onDrag = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            onDrag = false;
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (onDrag)
+        {
+            rigidbody2D.MovePosition(mainCamera.ScreenToWorldPoint(Input.mousePosition));
+        }
+    }
+}
---
+++ Assets/Script/DragDropUi.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragDropUi : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] private Canvas canvas = default;
+    [SerializeField] private Camera mainCamera = default;
+    private bool onCollider = false, onDrag = false;
+
+    void Update()
+    {
+        if (onCollider & Input.GetKeyDown(KeyCode.Mouse0))
+
+        {
+            onDrag = true;
+        }
+
+        if (onDrag)
+        {
+            transform.position = mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, canvas.planeDistance));
+        }
+
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            onDrag = false;
+        }
+    }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        onCollider = true;
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        onCollider = false;
+    }
+}
